82122e17db70b56fd6fa4ecd2a525b4b638145c0
oleg31947 <oleg31947@gmail.com>
2020-06-02 01:29:08 -0700

    Sign_in and Help_serch files

---
+++ features/steps/Help_serch.py
@@ -0,0 +1,23 @@
+from behave import given, when, then
+from selenium.webdriver.common.by import By
+
+MORE_SOLUSHEN_FILD = (By.ID, "helpsearch")
+GO = (By.XPATH, "//input[@class='a-button-input']")
+HEADER = (By.XPATH, "//a[@class='a-link-normal'and contains(text(),'Cancel Items or Orders')]")
+
+@given("Open Amazon Help page")
+def open_amazon(context):
+    context.driver.get("https://www.amazon.com/gp/help/customer/display.html")
+
+@when("Use Find more solutions field and search for {search_word}")
+def search_cancel(context, search_word):
+    search = context.driver.find_element(*MORE_SOLUSHEN_FILD)
+    search.clear()
+    search.send_keys(search_word)
+@when("Click Go")
+def click(context):
+    context.driver.find_element(*GO).click()
+@then("Verify that {necessary} text is present")
+def text_present(context, necessary):
+    text = context.driver.find_element(*HEADER).text
+    assert necessary == text, f'We expect {necessary} but got {text}'
---
+++ features/tests/Help_serch.feature
@@ -0,0 +1,10 @@
+# Created by oleg3 at 6/2/2020
+Feature: Help search
+  # Enter feature description here
+
+  Scenario: User can search for solutions of Cancelling an order on Amazon
+    Given Open Amazon Help page
+    When Use Find more solutions field and search for Cancel order
+    And Click Go
+    Then Verify that Cancel Items or Orders text is present
+
